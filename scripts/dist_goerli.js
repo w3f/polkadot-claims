b59ef3b1aca73116e412aede900a3f24b4aa798d
Logan Saether <x@logansaether.com>
2019-07-10 16:51:39 +0200

    intermediate

--- scripts/dist_goerli.js
+++ scripts/dist_goerli.js
@@ -1,11 +1,11 @@
 const Web3 = require('web3');
-// const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
 const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim(); // 0x7103ff712AB6CB0B995cEBcf3257833aF8357274
+const mnemonic = fs.readFileSync(".secret").toString().trim(); // 0x7103ff712AB6CB0B995cEBcf3257833aF8357274
 
-// const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://127.0.0.1:8546'))
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+const hdProvider = new HDWalletProvider(mnemonic, 'https://goerli.prylabs.net/');
+const web3 = new Web3(hdProvider);
 
 const FrozenToken = require('../build/contracts/FrozenToken.json');
 const Claims = require('../build/contracts/Claims.json');
@@ -16,7 +16,6 @@ const myUtils = require('./utils.js');
   const accounts = await web3.eth.getAccounts();
 
   const sender = accounts[0];
-  // const bal = await web3.eth.getBalance(sender);
 
   const netId = await web3.eth.net.getId();
 
@@ -31,17 +30,19 @@ const myUtils = require('./utils.js');
   let n = 150;
   let myAccounts = [];
   let distributionPromise = [];
+
   while (n > 0) {
     const account = web3.eth.accounts.create();
     myAccounts.push(account);
-    fs.appendFileSync('./data', JSON.stringify(account));
-    fs.appendFileSync('./addresses', account.address);
+    // fs.appendFileSync('./data', JSON.stringify(account));
+    // fs.appendFileSync('./addresses', account.address);
     if (n < 100) {
       distributionPromise.push(
         frozenToken.methods.transfer(account.address, '1').send({
           from: sender,
           gas: '333333',
-        }).on('transactionHash', (hash) => fs.appendFileSync('./txHashes', hash))
+        })
+        // .on('transactionHash', (hash) => fs.appendFileSync('./txHashes', hash))
       );
     } else {
       fs.appendFileSync('testAllocations.csv', account.address+','+'1'+'\n');
@@ -52,15 +53,16 @@ const myUtils = require('./utils.js');
   await Promise.all(distributionPromise);
 
   await Promise.all(myAccounts.map(async (account, index) => {
-    fs.appendFileSync('./privateKeys', account.privateKey + '\n');
+    // fs.appendFileSync('./privateKeys', account.privateKey + '\n');
+
+    await web3.eth.sendTransaction({
+      from: sender,
+      to: account.address,
+      value: '10000000000000000', 
+    });
 
     if (index % 2 == 0) {
-      await web3.eth.sendTransaction({
-        from: sender,
-        to: account.address,
-        value: '10000000000000000', 
-      });
-
+      fs.appendFileSync('./claimedPKs.txt', account.privateKey +'\n');
     
       const dot = myUtils.decodeToPubKey(myUtils.getPolkadotAddress(account.address.slice(0, 14)));
       const data = claims.methods.claim(account.address, dot).encodeABI();
@@ -73,6 +75,8 @@ const myUtils = require('./utils.js');
 
       return web3.eth.sendSignedTransaction(rawTx.rawTransaction).on('receipt', console.log);
     } else {
+      fs.appendFileSync('./unclaimedPKs.txt', account.privateKey +'\n');
+
       fs.appendFileSync('testVesting.csv', account.address +',');
       fs.appendFileSync('testIndices.csv', account.address +',');
       const amendedAccount = web3.eth.accounts.create();
